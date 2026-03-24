bd566faf4b9b2e3503b7772d9ca7e1c64d20234a
Eder Mamani <eder.mamani@opendeusto.es>
2026-03-24 14:22:01 +0100

    Ventana ataques arreglada Ahora se oculta la ventana y no tapa al resto

--- Frontend/JuegoRolPagina/src/app/app.routes.ts
+++ Frontend/JuegoRolPagina/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { CreadorHechizos } from './creador-hechizos/creador-hechizos';
 
 export const routes: Routes = [
-    { path: 'creador', component: CreadorHechizos }
+   
 ];
--- Frontend/JuegoRolPagina/src/app/creador-hechizos/creador-hechizos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-creador-hechizos',
-  imports: [],
-  templateUrl: './creador-hechizos.html',
-  styleUrl: './creador-hechizos.css',
-})
-export class CreadorHechizos {}
