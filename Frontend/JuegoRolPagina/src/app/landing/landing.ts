d00b786c9922426199123bca5d9443f73482535b
Jon Fernández <j.fernandezde@opendeusto.es>
2026-03-25 17:58:14 +0100

    Opción de unirte a partida añadida.

--- Frontend/JuegoRolPagina/src/app/landing/landing.ts
+++ Frontend/JuegoRolPagina/src/app/landing/landing.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {RouterLink, RouterModule} from '@angular/router';
+
+import { Partida } from '../models/partida';
 
 @Component({
   selector: 'app-landing',
@@ -8,4 +10,31 @@ import {RouterLink, RouterModule} from '@angular/router';
   styleUrl: './landing.css',
   imports: [RouterLink]
 })
-export class Landing {}
+// export class Landing {}
+
+export class Landing implements OnInit {
+
+  partidas: Partida[] = [];
+
+  ngOnInit(): void {
+    this.cargarPartidasMock();
+  }
+
+  cargarPartidasMock() {
+    this.partidas = [
+      {
+        nombre: 'Aventura en el bosque',
+        descripcion: 'Exploración y misterio',
+        idioma: 'ES',
+        maxJugadores: 4
+      },
+      {
+        nombre: 'Batalla final',
+        descripcion: 'PvP intenso',
+        idioma: 'EN',
+        maxJugadores: 8
+      }
+    ];
+  }
+
+}
